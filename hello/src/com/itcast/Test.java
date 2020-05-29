5fcdac7376d1fd747278dd9b8208749ab4bfe5a6
joke <aaaa.@163.com>
2020-05-29 14:29:30 +0800

    update by liweixiang

--- hello/src/com/itcast/Test.java
+++ hello/src/com/itcast/Test.java
@@ -3,5 +3,10 @@ package com.itcast;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        int i =0 ;
+        i++;
+        i++;
+
     }
 }
